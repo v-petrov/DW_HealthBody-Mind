9bf860bd16a899005026394a0ad93d3deeedfe0b
v-petrov <vapetrov@tu-sofia.bg>
2024-11-17 21:56:50 +0200

    structure of controllers and services added

---
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/controller/FoodController.java
@@ -0,0 +1,11 @@
+package com.diplomawork.healthbody.mind.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food")
+@RequiredArgsConstructor
+public class FoodController {
+}
