1aec702e01a1a3a32e2388f2fcff7bfb91251b68
v-petrov <vapetrov@tu-sofia.bg>
2025-02-17 00:04:24 +0200

    updated the GlobalExceptionHandler and added a new method in the UserRepository

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/exceptions/GlobalExceptionHandler.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/exceptions/GlobalExceptionHandler.java
@@ -2,22 +2,23 @@ package com.diplomawork.healthbody.mind.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(EmailValidation.class)
-    public ResponseEntity<String> existingEmailHandler(EmailValidation e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-    }
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> authenticationHandler(AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    public ResponseEntity<Map<String, String>> authenticationHandler(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Collections.singletonMap("message", e.getMessage()));
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> validationErrorHandler(MethodArgumentNotValidException e) {
+    public ResponseEntity<Map<String, String>> validationErrorHandler(MethodArgumentNotValidException e) {
         if (e.getBindingResult().hasErrors()) {
             String errorMessage = e.getBindingResult().getGlobalError() != null
                     ? e.getBindingResult().getGlobalError().getDefaultMessage()
@@ -25,8 +26,15 @@ public class GlobalExceptionHandler {
                     ? e.getBindingResult().getFieldError().getDefaultMessage()
                     : "Validation error occurred.");
 
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Collections.singletonMap("message", errorMessage));
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation error occurred.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Collections.singletonMap("message", "Validation error occurred."));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> badCredentialsHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Collections.singletonMap("message", "Invalid email or password"));
     }
 }
