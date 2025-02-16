a5636a77aac604c68dd5001f27ece27c4e27cdc1
v-petrov <vapetrov@tu-sofia.bg>
2025-02-17 00:02:15 +0200

    added logging and removed EmailValidation exception class

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/exceptions/EmailValidation.java
+++
@@ -1,7 +0,0 @@
-package com.diplomawork.healthbody.mind.exceptions;
-
-public class EmailValidation extends RuntimeException {
-    public EmailValidation(String message) {
-        super(message);
-    }
-}
