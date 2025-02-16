56b3792ef6cba8ce78d7d2455cd2402065df49ce
v-petrov <vapetrov@tu-sofia.bg>
2025-02-17 00:04:53 +0200

    changed the thrown exception

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/security/configuration/ApplicationConfig.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/security/configuration/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.diplomawork.healthbody.mind.security.configuration;
 
+import com.diplomawork.healthbody.mind.exceptions.AuthenticationException;
 import com.diplomawork.healthbody.mind.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,7 +21,7 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return email -> userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User couldn't be found!"));
+                .orElseThrow(() -> new AuthenticationException("Invalid email or password."));
     }
 
     @Bean
