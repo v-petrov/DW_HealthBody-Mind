ae9c4aabf3b1c0188c25e607ac7c05be65e0addd
v-petrov <vapetrov@tu-sofia.bg>
2025-02-18 23:56:25 +0200

    added a method for extracting the entity via user id

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/repository/NutritionsAndGoalsRepository.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/repository/NutritionsAndGoalsRepository.java
@@ -1,11 +1,14 @@
 package com.diplomawork.healthbody.mind.repository;
 
 import com.diplomawork.healthbody.mind.model.NutritionsAndGoals;
+import com.diplomawork.healthbody.mind.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NutritionsAndGoalsRepository extends JpaRepository<NutritionsAndGoals, Integer> {
+    Optional<NutritionsAndGoals> findByUserId(Integer userId);
 }
