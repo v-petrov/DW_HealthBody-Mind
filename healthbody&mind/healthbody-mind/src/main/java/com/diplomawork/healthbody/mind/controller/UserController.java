65e1cfe7d5d6961484f555de4144b5b12d7379eb
v-petrov <vapetrov@tu-sofia.bg>
2025-02-24 22:43:15 +0200

    updated user controller and service for the user's data manipulation

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/controller/UserController.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.diplomawork.healthbody.mind.controller;
 
 import com.diplomawork.healthbody.mind.dto.CaloriesDto;
+import com.diplomawork.healthbody.mind.dto.UserDataDto;
+import com.diplomawork.healthbody.mind.dto.UserProfileDto;
 import com.diplomawork.healthbody.mind.security.jwt.JwtService;
 import com.diplomawork.healthbody.mind.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
@@ -15,6 +23,7 @@ import java.util.Map;
 public class UserController {
     private final UserService userService;
     private final JwtService jwtService;
+    private static final String IMAGES_DIR = "images/";
 
     @GetMapping("/getCalories")
     private ResponseEntity<CaloriesDto> getUsersCalories(@RequestHeader("Authorization") String token) {
@@ -24,7 +33,77 @@ public class UserController {
 
         String cleanToken = token.replace("Bearer ", "");
         Integer userId = jwtService.extractUserId(cleanToken);
-        CaloriesDto caloriesDto= userService.getUserCalories(userId);
+        CaloriesDto caloriesDto = userService.getUserCalories(userId);
         return ResponseEntity.ok(caloriesDto);
+    }
+    @GetMapping("/getUserData")
+    private ResponseEntity<UserDataDto> getUserData(@RequestHeader("Authorization") String token) {
+        if (!token.startsWith("Bearer ")) {
+            throw new RuntimeException("Invalid token format.");
+        }
+
+        String cleanToken = token.replace("Bearer ", "");
+        Integer userId = jwtService.extractUserId(cleanToken);
+        UserDataDto userDataDto = userService.getUserData(userId);
+        return ResponseEntity.ok(userDataDto);
+    }
+    @PutMapping("/saveUserCalories")
+    private ResponseEntity<Map<String, String>> saveUserCalories(@RequestHeader("Authorization") String token, @Valid @RequestBody CaloriesDto caloriesDto) {
+        if (!token.startsWith("Bearer ")) {
+            throw new RuntimeException("Invalid token format.");
+        }
+
+        String cleanToken = token.replace("Bearer ", "");
+        Integer userId = jwtService.extractUserId(cleanToken);
+        userService.saveUserCalories(userId, caloriesDto);
+        return ResponseEntity.ok(Collections.singletonMap("validation", "Calories are saved successfully!"));
+    }
+    @PutMapping("/saveUserProfile")
+    private ResponseEntity<CaloriesDto> saveUserProfile(@RequestHeader("Authorization") String token, @Valid @RequestBody UserProfileDto userProfileDto) {
+        if (!token.startsWith("Bearer ")) {
+            throw new RuntimeException("Invalid token format.");
+        }
+
+        String cleanToken = token.replace("Bearer ", "");
+        Integer userId = jwtService.extractUserId(cleanToken);
+        CaloriesDto updatedUserCalories = userService.saveUserProfile(userId, userProfileDto);
+
+        return ResponseEntity.ok(updatedUserCalories);
+    }
+    @PutMapping("/updateProfilePicture")
+    private ResponseEntity<String> uploadProfilePicture(@RequestHeader("Authorization") String token, @RequestParam("file") MultipartFile file) {
+        if (!token.startsWith("Bearer ")) {
+            throw new RuntimeException("Invalid token format.");
+        }
+
+        String cleanToken = token.replace("Bearer ", "");
+        Integer userId = jwtService.extractUserId(cleanToken);
+        try {
+            if (file.isEmpty()) {
+                throw new RuntimeException("File is empty!");
+            }
+            Files.createDirectories(Paths.get(IMAGES_DIR));
+            String filename = "profilePictureOfUserWithId_" + userId + "_" + file.getOriginalFilename();
+            Path filePath = Paths.get(IMAGES_DIR, filename);
+            Files.write(filePath, file.getBytes());
+            String imageUrl = "/images/" + filename;
+            userService.updateProfilePicture(userId, imageUrl);
+
+            return ResponseEntity.ok(imageUrl);
+        } catch (Exception e) {
+            throw new RuntimeException("Error with processing the image!");
+        }
+    }
+    @GetMapping("/getProfilePicture")
+    private ResponseEntity<String> getProfilePicture(@RequestHeader("Authorization") String token) {
+        if (!token.startsWith("Bearer ")) {
+            throw new RuntimeException("Invalid token format.");
+        }
+
+        String cleanToken = token.replace("Bearer ", "");
+        Integer userId = jwtService.extractUserId(cleanToken);
+        String imageUrl = userService.getProfilePicture(userId);
+
+        return ResponseEntity.ok(imageUrl);
     }
 }
--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/service/UserService.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/service/UserService.java
@@ -1,13 +1,19 @@
 package com.diplomawork.healthbody.mind.service;
 
 import com.diplomawork.healthbody.mind.dto.CaloriesDto;
+import com.diplomawork.healthbody.mind.dto.UserDataDto;
+import com.diplomawork.healthbody.mind.dto.UserProfileDto;
+import com.diplomawork.healthbody.mind.exceptions.UserNotFoundException;
 import com.diplomawork.healthbody.mind.model.NutritionsAndGoals;
 import com.diplomawork.healthbody.mind.model.User;
 import com.diplomawork.healthbody.mind.model.UserProfile;
 import com.diplomawork.healthbody.mind.model.enums.ActivityLevel;
 import com.diplomawork.healthbody.mind.model.enums.Gender;
+import com.diplomawork.healthbody.mind.model.enums.Goal;
 import com.diplomawork.healthbody.mind.model.enums.WeeklyGoal;
 import com.diplomawork.healthbody.mind.repository.NutritionsAndGoalsRepository;
+import com.diplomawork.healthbody.mind.repository.UserProfileRepository;
+import com.diplomawork.healthbody.mind.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +25,8 @@ import java.time.Period;
 @RequiredArgsConstructor
 public class UserService {
     private final NutritionsAndGoalsRepository nutritionsAndGoalsRepository;
+    private final UserRepository userRepository;
+    private final UserProfileRepository userProfileRepository;
 
     public CaloriesDto getUserCalories(Integer userId) {
         NutritionsAndGoals caloriesGoals = nutritionsAndGoalsRepository.findByUserId(userId)
@@ -27,9 +35,10 @@ public class UserService {
                 .calories(caloriesGoals.getCaloriesGoal())
                 .protein(caloriesGoals.getProteinGoal())
                 .carbs(caloriesGoals.getCarbsGoal())
-                .fats(caloriesGoals.getFatGoal()).build();
+                .fats(caloriesGoals.getFatGoal())
+                .water(Double.parseDouble(String.valueOf(caloriesGoals.getWaterGoal()))).build();
     }
-    public void calculateUserCalories(UserProfile userProfile, User user) {
+    public void calculateUserCalories(UserProfile userProfile, Integer userId, boolean flag, double water) {
         BigDecimal weight = userProfile.getWeight();
         int height = userProfile.getHeight();
         int age  = Period.between(userProfile.getDateOfBirth(), LocalDate.now()).getYears();
@@ -63,9 +72,21 @@ public class UserService {
         int fats = (int) ((calories * 0.25) / 9.0);
         int carbs = (int) ((calories * 0.50) / 4.0);
 
-        saveUserCaloriesAndGoals(user, (int)calories, protein, fats, carbs);
+        if(flag) {
+            saveUserCaloriesAndGoals(userId, (int)calories, protein, fats, carbs);
+        } else {
+            CaloriesDto caloriesDto = CaloriesDto.builder()
+                    .calories((int)calories)
+                    .protein(protein)
+                    .carbs(carbs)
+                    .fats(fats)
+                    .water(water).build();
+            saveUserCalories(userId, caloriesDto);
+        }
     }
-    private void saveUserCaloriesAndGoals(User user, int calories, int protein, int fats, int carbs) {
+    private void saveUserCaloriesAndGoals(Integer userId, int calories, int protein, int fats, int carbs) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User couldn't be found!"));
         NutritionsAndGoals nutritionsAndGoals = NutritionsAndGoals.builder()
                 .caloriesGoal(calories)
                 .proteinGoal(protein)
@@ -75,5 +96,78 @@ public class UserService {
                 .stepGoal(8000)
                 .user(user).build();
         nutritionsAndGoalsRepository.save(nutritionsAndGoals);
+    }
+    public UserDataDto getUserData(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User couldn't be found!"));
+        UserProfile userProfile = userProfileRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("User profile couldn't be found!"));
+        NutritionsAndGoals nutritionsAndGoals = nutritionsAndGoalsRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("User goals couldn't be found!"));
+        return UserDataDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .gender(String.valueOf(userProfile.getGender()))
+                .dateOfBirth(String.valueOf(userProfile.getDateOfBirth()))
+                .height(userProfile.getHeight())
+                .weight(Double.parseDouble(String.valueOf(userProfile.getWeight())))
+                .goalWeight(Double.parseDouble(String.valueOf(userProfile.getGoalWeight())))
+                .goal(String.valueOf(userProfile.getGoal()))
+                .weeklyGoal(String.valueOf(userProfile.getWeeklyGoal()))
+                .activityLevel(String.valueOf(userProfile.getActivityLevel()))
+                .steps(nutritionsAndGoals.getStepGoal())
+                .calories(nutritionsAndGoals.getCaloriesGoal())
+                .protein(nutritionsAndGoals.getProteinGoal())
+                .carbs(nutritionsAndGoals.getCarbsGoal())
+                .fats(nutritionsAndGoals.getFatGoal())
+                .water(Double.parseDouble(String.valueOf(nutritionsAndGoals.getWaterGoal())))
+                .build();
+    }
+    public void saveUserCalories(Integer userId, CaloriesDto caloriesDto) {
+        NutritionsAndGoals nutritionsAndGoals = nutritionsAndGoalsRepository.findByUserId(userId).
+                orElseThrow(() -> new RuntimeException("User goals couldn't be found!"));
+
+        nutritionsAndGoals.setCaloriesGoal(caloriesDto.getCalories());
+        nutritionsAndGoals.setCarbsGoal(caloriesDto.getCarbs());
+        nutritionsAndGoals.setProteinGoal(caloriesDto.getProtein());
+        nutritionsAndGoals.setFatGoal(caloriesDto.getFats());
+        nutritionsAndGoals.setWaterGoal(BigDecimal.valueOf(caloriesDto.getWater()));
+        nutritionsAndGoalsRepository.save(nutritionsAndGoals);
+    }
+    public CaloriesDto saveUserProfile(Integer userId, UserProfileDto userProfileDto) {
+        UserProfile userProfile = userProfileRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("User profile couldn't be found!"));
+        userProfile.setWeight(BigDecimal.valueOf(userProfileDto.getWeight()));
+        userProfile.setGoalWeight(BigDecimal.valueOf(userProfileDto.getGoalWeight()));
+        userProfile.setGoal(Goal.valueOf(userProfileDto.getGoal()));
+        userProfile.setWeeklyGoal(WeeklyGoal.valueOf(userProfileDto.getWeeklyGoal()));
+        userProfile.setActivityLevel(ActivityLevel.valueOf(userProfileDto.getActivityLevel()));
+        userProfileRepository.save(userProfile);
+
+        NutritionsAndGoals nutritionsAndGoals = nutritionsAndGoalsRepository.findByUserId(userId).
+                orElseThrow(() -> new RuntimeException("User goals couldn't be found!"));
+        nutritionsAndGoals.setStepGoal(userProfileDto.getSteps());
+        nutritionsAndGoalsRepository.save(nutritionsAndGoals);
+
+        if(userProfileDto.isStepsFlag()) {
+            calculateUserCalories(userProfile, userId, false, Double.parseDouble(String.valueOf(nutritionsAndGoals.getWaterGoal())));
+        }
+        return getUserCalories(userId);
+    }
+    public void updateProfilePicture(Integer userId, String imageUrl) {
+        UserProfile userProfile = userProfileRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("User's profile couldn't be found!"));
+        userProfile.setImageUrl(imageUrl);
+        userProfileRepository.save(userProfile);
+    }
+    public String getProfilePicture(Integer userId) {
+        UserProfile userProfile = userProfileRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("User's profile couldn't be found!"));
+
+        if (userProfile.getImageUrl() == null || userProfile.getImageUrl().isEmpty()) {
+            throw new RuntimeException("No profile picture found.");
+        }
+
+        return  userProfile.getImageUrl();
     }
 }
