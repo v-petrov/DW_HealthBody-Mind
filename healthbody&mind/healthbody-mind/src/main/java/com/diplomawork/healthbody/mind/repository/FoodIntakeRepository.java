c218548a6f2b3a80bb68d91f2759b07f6bd1ebd7
v-petrov <vapetrov@tu-sofia.bg>
2025-03-09 12:45:43 +0200

    added a method for finding foodIntakes by user and date, the last commit was a method for finding a food by name in the food repository

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/repository/FoodIntakeRepository.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/repository/FoodIntakeRepository.java
@@ -1,9 +1,14 @@
 package com.diplomawork.healthbody.mind.repository;
 
 import com.diplomawork.healthbody.mind.model.FoodIntake;
+import com.diplomawork.healthbody.mind.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface FoodIntakeRepository extends JpaRepository<FoodIntake, Long> {
+    List<FoodIntake> findByUserAndDate(User user, LocalDate date);
 }
