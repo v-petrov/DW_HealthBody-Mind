aca1e92bd5e5a7f7e1c5363ffdfaa37d414b9aba
v-petrov <vapetrov@tu-sofia.bg>
2025-03-09 12:44:42 +0200

    added a method for finding foodIntakes by user and date

--- healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/repository/FoodRepository.java
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/repository/FoodRepository.java
@@ -4,8 +4,9 @@ import com.diplomawork.healthbody.mind.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.List;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Integer> {
+    List<Food> findByNameContainingIgnoreCase(String name);
 }
