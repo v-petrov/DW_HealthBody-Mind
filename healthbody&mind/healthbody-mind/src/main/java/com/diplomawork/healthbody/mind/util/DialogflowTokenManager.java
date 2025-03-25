579d70e7912bd71da97bccf1ebee419fc828093b
v-petrov <vapetrov@tu-sofia.bg>
2025-03-25 23:34:35 +0200

    added token manager and json file for generating token for dialogflow

---
+++ healthbody&mind/healthbody-mind/src/main/java/com/diplomawork/healthbody/mind/util/DialogflowTokenManager.java
@@ -0,0 +1,29 @@
+package com.diplomawork.healthbody.mind.util;
+
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class DialogflowTokenManager {
+
+    private static final String DIALOGFLOW_SCOPE = "https://www.googleapis.com/auth/cloud-platform";
+    private static final String KEY_FILE_PATH = "src/main/resources/healthbody-mind-1d21ce7a0fba.json";
+
+    private AccessToken cachedToken;
+
+    public String getAccessToken() throws IOException {
+        if (cachedToken == null || cachedToken.getExpirationTime().before(new java.util.Date())) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(KEY_FILE_PATH))
+                    .createScoped(Collections.singleton(DIALOGFLOW_SCOPE));
+            credentials.refreshIfExpired();
+            cachedToken = credentials.getAccessToken();
+        }
+
+        return cachedToken.getTokenValue();
+    }
+}
